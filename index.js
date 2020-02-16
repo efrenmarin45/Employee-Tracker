f56eadf26782dc7f6fbf429e631af4e313da8835
Efren_Marin <efren45marin@gmail.com>
2020-02-16 01:59:36 -0800

    Added initial functions for inquirer.

--- index.js
+++ index.js
@@ -20,7 +20,62 @@ connection.connect(function(err) {
 
 function initiate(){
     inquirer
-        .prompt({
-            
+        .prompt([
+            {
+            name: "userSelect",
+            type: "list",
+            message: "Please choose from the actions listed to proceed.",
+            choices: ["View an entry", "Add an entry", "Update an entry"]
+            }
+        ])
+        .then(answer => {
+            if (answer.userSelect === "View an entry"){
+                viewEntry();
+            }
+            else if (answer.userSelect === "Add an entry"){
+                addEntry();
+            }
+            else if (answer.userSelect === "Update an entry"){
+                updateEntry();
+            }
+            else {
+                connection.end();
+            }
+        });
+}
+
+function viewEntry(){
+    inquirer
+        .prompt([
+            {
+                name: "userView",
+                type: "list",
+                message: "What would you like to view?",
+                choices: ["View departments", "View roles", "View employees", 
+                "View employees by department", "View employees by role", "Return to menu"]
+            }
+        ])
+        .then(answer => {
+            if (answer.userView === "View departments"){
+                viewDepartment();
+            }
+            else if (answer.userView === "View roles"){
+                viewRoles();
+            }
+            else if (answer.userView === "View employees"){
+                viewEmployees();
+            }
+            else if (answer.userView === "View employees by department"){
+                employeeDepartment();
+            }
+            else if (answer.userView === "View employees by role"){
+                employeeRole();
+            }
+            else if (answer.userView === "Return to menu"){
+                initiate();
+            }
+            else{
+                connection.end();
+            }
         })
 }
