c316a1017ce3fc9ae6b82d29634531035cf257c7
Efren_Marin <efren45marin@gmail.com>
2020-02-17 00:05:18 -0800

    Removed several npm packages and replaced with others. Also worked on initial questions for user.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const consoleTable = require("console-table");
+const consoleTable = require("console.table");
 const inquirer = require("inquirer");
 const mysql = require("mysql");
-const cliArt = require("ascii-art");
+const cliArt = require("figlet");
+const clear = require("clear");
+const chalk = require("chalk");
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -14,8 +16,14 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
 
+    clear();
+
+    console.log(chalk.green(cliArt.textSync('Employee Tracker', {horizontalLayout: 'fitted'})));
+
     initiate();
 });
+
+
 
 
 function initiate(){
@@ -72,6 +80,64 @@ function viewEntry(){
                 employeeRole();
             }
             else if (answer.userView === "Return to menu"){
+                initiate();
+            }
+            else{
+                connection.end();
+            }
+        })
+}
+
+function addEntry(){
+    inquirer
+        .prompt([
+            {
+                name: "userAdd",
+                type: "list",
+                message: "What would you like to add?",
+                choices: ["Add a department", "Add an employee", "Add a job role", "Return to menu"]
+            }
+        ])
+        .then(answer => {
+            if (answer.userAdd === "Add a department"){
+                addDepartment();
+            }
+            else if (answer.userAdd === "Add an employee"){
+                addEmployee();
+            }
+            else if (answer.userAdd === "Add a job role"){
+                addRole();
+            }
+            else if (answer.userAdd === "Return to menu"){
+                initiate();
+            }
+            else{
+                connection.end();
+            }
+        })
+}
+
+function updateEntry(){
+    inquirer
+        .prompt([
+            {
+                name: "userUpdate",
+                type: "list",
+                message: "What would you like to update?",
+                choices: ["Update a department", "Update an employee", "Update a job role", "Return to menu"]
+            }
+        ])
+        .then(answer => {
+            if (answer.userUpdate === "Update a department"){
+                updateDepartment();
+            }
+            else if (answer.userUpdate === "Update an employee"){
+                updateEmployee();
+            }
+            else if (answer.userUpdate === "Update a job role"){
+                updateRole();
+            }
+            else if (answer.userUpdate === "Return to menu"){
                 initiate();
             }
             else{
